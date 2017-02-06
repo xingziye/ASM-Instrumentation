47866eea23c9835b997621fd6824a04382e195fc
Ziye Xing <xingziye46@gmail.com>
2017-02-06 09:38:05 -0800

    Complete framework

--- ASM/src/BankTransactions.java
+++ ASM/src/BankTransactions.java
@@ -6,13 +6,17 @@ public class BankTransactions {
 		for (int i = 0; i < 100; i++) {
 		    String accountId = "account" + i;
 		    bank.login("password", accountId, "Ashley");
-		    //bank.unimportantProcessing(accountId);
+		    bank.unimportantProcessing(accountId);
 		    bank.withdraw(accountId, Double.valueOf(i));
 		}
 		System.out.println("Transactions completed");
 
 	}
 	
+	private void unimportantProcessing(String accountId) {
+		//System.out.println(accountId);
+	}
+
 	@ImportantLog(fields = { "1", "2" })
 	public void login(String password, String accountId, String userName) {
 	    // login logic
